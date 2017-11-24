76b5fad9973499d2dffaeb76f8ebd3347cd8efc1
vbagustinus <vbagustinus@gmail.com>
2017-11-24 14:59:48 +0700

    fix simple menu

--- client/assets/js/datavue.js
+++ client/assets/js/datavue.js
@@ -124,5 +124,25 @@ Vue.use(VueGoogleMaps, {
         };
       });
       Vue.$gmapDefaultResizeBus.$emit('resize');
+      window.fbAsyncInit = function() {
+          FB.init({
+            appId      : '351979128600634',
+            xfbml      : true,
+            version    : 'v2.8'
+          });
+
+          //This function should be here, inside window.fbAsyncInit
+          FB.getLoginStatus(function(response) {
+            console.log(response);
+         });
+
+       };
+       (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
     }
   })
